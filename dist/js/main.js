feaa9d0c64fcfa7e7bf6bb14f26b7df548d9d13a
Umukoro Ovie <umukoro006@gmail.com>
2020-08-27 11:31:01 +0100

    menu button javascript and x button

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,9 @@
 // Select Dom Items
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of The Menu
 let showMenu = false;
@@ -21,6 +21,12 @@ function toggleMenu() {
     // Set Menu State
     showMenu = true;
   } else {
-
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+    // Set Menu state to false.
+    showMenu = false;
   }
 }
